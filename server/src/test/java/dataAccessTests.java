862fd0137cb64793ef28774b1be8c3fe36a583e0
Eli Ellsworth <bentalizac@gmail.com>
2024-03-07 14:52:38 -0700

    Rename test file

--- server/src/test/java/dataAccessTests.java
+++ server/src/test/java/dataAccessTests.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class SQLDataAccessTests {
+public class dataAccessTests {
 
     private DataAccess getDataAccess() throws ResponseException {
         return new MySQLDataAccess();
