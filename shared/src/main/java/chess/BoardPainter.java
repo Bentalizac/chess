5131d0c23d442d903938c3b0f1191ab4e7217186
Eli Ellsworth <bentalizac@gmail.com>
2024-04-11 21:57:47 -0600

    Style guide updates

--- shared/src/main/java/chess/BoardPainter.java
+++ shared/src/main/java/chess/BoardPainter.java
@@ -35,8 +35,12 @@ public class BoardPainter {
         return output + SET_BG_COLOR_DARK_GREY;
     }
 
-    private String buildSquare(int x, int y){
+    private String buildSquare(int x, int y, boolean isHighLight){
         StringBuilder output = new StringBuilder();
+        if (isHighLight){
+            output.append(buildHighlightSquare(x, y));
+            return output.toString();
+        }
         if(y%2 == 0) { // Even rows
             if(x%2 == 0){
                 output.append(buildBlackSquare(x, y));
@@ -61,7 +65,7 @@ public class BoardPainter {
         for (int y = 1; y < 9; y++) {
             output.append(" ").append(y).append(" ");
             for (int x = 8; x >= 1; x--) {
-                output.append(buildSquare(x, y));
+                output.append(buildSquare(x, y, false));
                 output.append("\n" + SET_BG_COLOR_DARK_GREY);
             }
         }
@@ -78,7 +82,7 @@ public class BoardPainter {
             output.append(" ").append(y).append(" ");
 
             for (int x = 1; x < 9; x++){
-                output.append(buildSquare(x, y));
+                output.append(buildSquare(x, y, false) );
             }
 
             output.append("\n" + SET_BG_COLOR_DARK_GREY);
@@ -91,75 +95,24 @@ public class BoardPainter {
         var validMoves = board.getPieceMoves(position);
         var endpositions = new ArrayList<ChessPosition>();
         var color = game.getTeamTurn();
-        for(ChessMove move : validMoves) {
-            endpositions.add(move.getEndPosition());}
-        StringBuilder output = new StringBuilder("\n");
-        if (color == ChessGame.TeamColor.WHITE){
-            for (int y = 8; y >= 1; y--){
-                output.append(" ").append(y).append(" ");
-                for (int x = 1; x < 9; x++){
-                    if(y%2 == 0) { // Even rows
-                        if(x%2 == 0){
-                            if(endpositions.contains(new ChessPosition(x, y))) {
-                                output.append(buildHighlightSquare(x, y));}
-                            else {
-                                output.append(buildBlackSquare(x, y));}}
-                        else{
-                            if(endpositions.contains(new ChessPosition(x, y))) {
-                                output.append(buildHighlightSquare(x, y));}
-                            else {
-                                output.append(buildWhiteSquare(x, y));}}}
-                    else{
-                        if(x%2 == 1){
-                            if(endpositions.contains(new ChessPosition(x, y))) {
-                                output.append(buildHighlightSquare(x, y));}
-                            else {
-                                output.append(buildBlackSquare(x, y));}}
-                        else{
-                            if(endpositions.contains(new ChessPosition(x, y))) {
-                                output.append(buildHighlightSquare(x, y));
-                            }
-                            else {
-                                output.append(buildWhiteSquare(x, y));}}}}
-                output.append("\n" + SET_BG_COLOR_DARK_GREY);
-            }
-            output.append("    a  b  c  d  e  f  g  h \n");
+        for (ChessMove move : validMoves) {
+            endpositions.add(move.getEndPosition());
         }
-        else{
-            for (int y = 1; y <9; y++){
-                output.append(" ").append(y).append(" ");
-                for (int x = 8; x >= 1; x--){
-                    if(y%2 == 0) { // Even rows
-                        if(x%2 == 0){
-                            if(endpositions.contains(new ChessPosition(x, y))) {
-                                output.append(buildHighlightSquare(x, y));
-                            }
-                            else {
-                                output.append(buildBlackSquare(x, y));
-                            }
-                        }
-                        else{
-                            if(endpositions.contains(new ChessPosition(x, y))) {
-                                output.append(buildHighlightSquare(x, y));
-                            }
-                            else {
-                                output.append(buildWhiteSquare(x, y));}}}
-                    else{
-                        if(x%2 == 1){
-                            if(endpositions.contains(new ChessPosition(x, y))) {
-                                output.append(buildHighlightSquare(x, y));}
-                            else {
-                                output.append(buildBlackSquare(x, y));}}
-                        else{
-                            if(endpositions.contains(new ChessPosition(x, y))) {
-                                output.append(buildHighlightSquare(x, y));
-                            }
-                            else {
-                                output.append(buildWhiteSquare(x, y));}}}}
-                output.append("\n" + SET_BG_COLOR_DARK_GREY);
+        StringBuilder output = new StringBuilder("\n");
+        for (int y = 8; y >= 1; y--) {
+            output.append(" ").append(y).append(" ");
+            for (int x = (color == ChessGame.TeamColor.WHITE) ? 1 : 8;
+                 (color == ChessGame.TeamColor.WHITE) ? x < 9 : x >= 1;
+                 x += (color == ChessGame.TeamColor.WHITE) ? 1 : -1) {
+                ChessPosition currentPos = new ChessPosition(x, y);
+                output.append(buildSquare(x, y, endpositions.contains(currentPos)));
             }
-            output.append("    h  g  f  e  d  c  b  a \n");}
+            output.append("\n" + SET_BG_COLOR_DARK_GREY);
+        }
+        output.append("    ");
+        output.append((color == ChessGame.TeamColor.WHITE) ? "a  b  c  d  e  f  g  h \n" : "h  g  f  e  d  c  b  a \n");
         System.out.print(output);
     }
+
 
 }
