28d86d4044e75b484c481c87e8562d4d0bd0b1bd
Eli Ellsworth <bentalizac@gmail.com>
2024-04-12 17:16:14 -0600

    All test cases passing

--- client/src/main/java/Facades/NotificationHandler.java
+++ client/src/main/java/Facades/NotificationHandler.java
@@ -11,17 +11,12 @@ import webSocketMessages.serverMessages.ServerMessage;
 public class NotificationHandler {
     public void notify(Notification notification) {
         System.out.println(notification.getMessage());
-        //printPrompt();
     }
 
     public void loadGame(LoadGameMessage input) {
         System.out.println(input.getMessage());
         drawBoard(input, input.getColor());
     }
-
-    //private void printPrompt() {
-    //    System.out.print("\n"  + ">>> ");
-    //}
 
     private void drawBoard(Notification input, ChessGame.TeamColor color){
         ChessGame game = new Gson().fromJson(input.getGame(), ChessGame.class);
